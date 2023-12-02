d141a57d944669ff276690f59ccd450667196103
Jacob Rodal <jrodal@meta.com>
2023-12-01 19:35:11 -0800

    day 2 skeleton

---
+++ day2/day2.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# www.jrodal.com
+
+import re
+
+
+def part1(s: str) -> int:
+    return 0
+
+
+def part2(s: str) -> int:
+    return 0
+
+
+if __name__ == "__main__":
+    import sys
+
+    with open(sys.argv[1]) as f:
+        s = f.read().strip()
+
+    print(f"Part 1: {part1(s)}")
+    print(f"Part 2: {part2(s)}")
