bd9683b789d0f87d87d0a1ec17f20745b34e95a4
Jacob Rodal <dev+github@jrodal.com>
2023-12-08 22:01:16 -0800

    part1 refactored

--- py2023/day9/solution.py
+++ py2023/day9/solution.py
@@ -10,32 +10,20 @@ class Solver(BaseSolver):
         res = 0
         for line in self.data.splitlines():
             history = np.array([int(i) for i in line.split()])
-            differences = np.diff(history)
-            all_zero = np.all(differences == 0)
-            last_digits = [history[-1]]
-            while not all_zero:
-                history = differences
-                differences = np.diff(history)
-                all_zero = np.all(differences == 0)
-                last_digits.append(history[-1])
-            last_digits.append(0)
-            cumsum = np.cumsum(sorted(last_digits))
-            res += cumsum[-1]
+            res += history[-1]
+            while not np.all(history == 0):
+                history = np.diff(history)
+                res += history[-1]
         return res
 
     def _part2(self) -> Solution:
         res = 0
         for line in self.data.splitlines():
             history = np.array([int(i) for i in line.split()])
-            differences = np.diff(history)
-            all_zero = np.all(differences == 0)
             first_digits = [history[0]]
-            while not all_zero:
-                history = differences
-                differences = np.diff(history)
-                all_zero = np.all(differences == 0)
+            while not np.all(history == 0):
+                history = np.diff(history)
                 first_digits.append(history[0])
-
             last_digit = 0
             for digit in first_digits[::-1]:
                 last_digit = digit - last_digit
