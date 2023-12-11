c0c473b44cd3f8d8044d900b39dab2693a10c689
Jacob Rodal <dev+github@jrodal.com>
2023-12-10 21:52:24 -0800

    day 11 refactored

--- py2023/day11/solution.py
+++ py2023/day11/solution.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 # www.jrodal.com
 
+from typing import Iterator
 from aoc_utils.base_solver import BaseSolver, Solution
 from aoc_utils.grid import Grid
 
@@ -8,42 +9,35 @@ from itertools import combinations
 
 
 class Solver(BaseSolver):
-    def _get_galaxy_positions(self, expansion: int = 2) -> list[tuple[int, int]]:
+    def _part1(self) -> Solution:
+        return self._calculate_sum_of_shortest_paths(expansion=2)
+
+    def _part2(self) -> Solution:
+        return self._calculate_sum_of_shortest_paths(expansion=1_000_000)
+
+    def _calculate_sum_of_shortest_paths(self, expansion: int) -> int:
+        res = 0
+        for (x1, y1), (x2, y2) in combinations(
+            self._get_galaxy_positions(expansion), 2
+        ):
+            res += abs(x1 - x2) + abs(y1 - y2)
+        return res
+
+    def _get_galaxy_positions(self, expansion: int = 2) -> Iterator[tuple[int, int]]:
         grid = Grid.from_lines(self.data)
-        grid_t = grid.transpose()
-        y_translation = [0 for y in range(grid.h)]
-        for y, row in enumerate(grid.rows):
-            if "#" not in row:
-                for j in range(y, grid.h):
-                    y_translation[j] += expansion - 1
+        x_translation = [0] * grid.w
+        y_translation = [0] * grid.h
+        for g, translation in (
+            (grid, y_translation),
+            (grid.transpose(), x_translation),
+        ):
+            for y, row in enumerate(g.rows):
+                if "#" not in row:
+                    for j in range(y, g.h):
+                        translation[j] += expansion - 1
 
-        x_translation = [0 for x in range(grid_t.h)]
-        for y, row in enumerate(grid_t.rows):
-            if "#" not in row:
-                for j in range(y, grid_t.h):
-                    x_translation[j] += expansion - 1
-
-        galaxy_positions = []
         for y in range(grid.h):
             for x in range(grid.w):
                 cell = grid.at(x, y)
                 if cell == "#":
-                    galaxy_positions.append(
-                        (x + x_translation[x], y + y_translation[y])
-                    )
-
-        return galaxy_positions
-
-    def _part1(self) -> Solution:
-        galaxy_positions = self._get_galaxy_positions()
-        res = 0
-        for (x1, y1), (x2, y2) in combinations(galaxy_positions, 2):
-            res += abs(x1 - x2) + abs(y1 - y2)
-        return res
-
-    def _part2(self) -> Solution:
-        galaxy_positions = self._get_galaxy_positions(1_000_000)
-        res = 0
-        for (x1, y1), (x2, y2) in combinations(galaxy_positions, 2):
-            res += abs(x1 - x2) + abs(y1 - y2)
-        return res
+                    yield x + x_translation[x], y + y_translation[y]
