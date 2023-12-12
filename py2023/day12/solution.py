74ccfb4669163ceaf5d8047b4899e89d82f9f5b9
Jacob Rodal <dev+github@jrodal.com>
2023-12-12 00:53:05 -0800

    part2

--- py2023/day12/solution.py
+++ py2023/day12/solution.py
@@ -2,10 +2,11 @@
 # www.jrodal.com
 
 
+from functools import cache
 from aoc_utils.base_solver import BaseSolver, Solution
 
 
-# @cache
+@cache
 def num_valid(
     record: str,
     nums: tuple[int, ...],
@@ -60,8 +61,14 @@ def num_valid(
 
     if n_damaged != target:
         return 0
-
-    return 1
+    if len(nums) > 1:
+        return 0
+    elif len(nums) == 1:
+        return int(nums[0] == n_damaged)
+    elif n_damaged == 0:
+        return 1
+    else:
+        return 0
 
 
 class Solver(BaseSolver):
