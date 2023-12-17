6b35fd6fceab17639ca19b4599e5b1064c639253
Jacob Rodal <dev+github@jrodal.com>
2023-12-17 00:44:33 -0800

    d17 checkpoint

--- py2023/day17/solution.py
+++ py2023/day17/solution.py
@@ -9,11 +9,18 @@ from collections import deque
 class Solver(BaseSolver):
     def _part1(self) -> Solution:
         grid = self.grid.transform(lambda x: int(x))
+        self._set_animation_grid(self.grid)
         state = ((Point(0, 0), Direction.RIGHT, 0), 0)
         seen = {}
+        seen_no_dir = {}
         queue = deque([state])
-        best_so_far = sum(grid.data)
+        best_so_far = 1050
+        skipped = 0
+        not_skipped = 0
+        paths_found = 0
+        i = 0
         while queue:
+            i += 1
             state_key, current_cost = queue.popleft()
             if current_cost >= best_so_far:
                 continue
@@ -25,16 +32,65 @@ class Solver(BaseSolver):
                 current_steps_straight,
             ) = state_key
 
-            seen[state_key] = current_cost
+            distance_to_finish = current_pos.manhattan_distance(
+                Point(grid.w - 1, grid.h - 1)
+            )
+
+            if (
+                distance_to_finish > 30
+                and current_pos in seen_no_dir
+                and current_cost > seen_no_dir[current_pos] + 15
+            ):
+                seen[state_key] = 10000
+                skipped += 1
+                continue
+
+            self._update_animation(point=current_pos, refresh=len(queue) % 500 == 0)
+
             if current_pos == Point(grid.w - 1, grid.h - 1):
+                paths_found += 1
                 new_cost = current_cost
                 best_so_far = min(best_so_far, new_cost)
                 seen[state_key] = best_so_far
                 continue
 
+            factor = 1
+            if 100 < current_cost < 200:
+                factor = 4
+            elif 300 < current_cost < 600:
+                factor = 3
+            elif current_cost < 600:
+                factor = 2
+
+            best_possible_score = current_cost + distance_to_finish * factor
+            if best_possible_score > best_so_far:
+                # best_so_far = possible_score
+                skipped += 1
+                seen[state_key] = best_possible_score
+                continue
+            else:
+                # print("Possible score: ", best_possible_score)
+                pass
+
+            not_skipped += 1
+            if i % 10000 == 0:
+                print(
+                    f"{len(queue)}, {paths_found=}, {best_so_far=}, {skipped=}, {not_skipped=}, {current_cost=}, {distance_to_finish=}"
+                )
+
+            seen[state_key] = current_cost
+            seen_no_dir[current_pos] = current_cost
+
             dirs = [current_dir.clockwise, current_dir.counter_clockwise]
             if current_steps_straight < 3:
                 dirs.append(current_dir)
+
+            if Direction.DOWN in dirs:
+                dirs.remove(Direction.DOWN)
+                dirs.insert(0, Direction.DOWN)
+            if Direction.RIGHT in dirs:
+                dirs.remove(Direction.RIGHT)
+                dirs.insert(0, Direction.RIGHT)
 
             for d in dirs:
                 next_pos = current_pos.neighbor(d)
