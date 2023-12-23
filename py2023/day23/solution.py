c952d5fbf5587b1b5ac1950ff6d2ebde828ac62a
Jacob Rodal <dev+github@jrodal.com>
2023-12-23 00:09:07 -0800

    day 23 part 2

--- py2023/day23/solution.py
+++ py2023/day23/solution.py
@@ -3,6 +3,8 @@
 
 from aoc_utils.base_solver import BaseSolver, Solution
 from aoc_utils.grid import Grid, Point
+import networkx as nx
+from collections import deque
 
 import sys
 
@@ -10,7 +12,6 @@ sys.setrecursionlimit(100000000)
 
 
 def steps_to_end(grid: Grid[str], current_position: Point, path: set[Point]) -> int:
-    assert current_position not in path
     if current_position == Point(grid.w - 2, grid.h - 1):
         return 0
     path.add(current_position)
@@ -46,38 +47,44 @@ def steps_to_end(grid: Grid[str], current_position: Point, path: set[Point]) -> 
     return best_so_far
 
 
-def steps_to_end_p2(
-    grid: Grid[str], current_position: Point, visited: Grid[bool], steps: int = 0
-) -> int:
-    if current_position == Point(grid.w - 2, grid.h - 1):
-        return steps
-
-    possible_positions = [
-        current_position.up,
-        current_position.down,
-        current_position.left,
-        current_position.right,
-    ]
-
-    best_so_far = -100000000000000
-
-    visited.replace(current_position, True)
-    for new_position in possible_positions:
-        if not visited.get(new_position, True) and grid.get(new_position, "#") != "#":
-            best_so_far = max(
-                best_so_far,
-                steps_to_end_p2(grid, new_position, visited, steps + 1),
-            )
-    visited.replace(current_position, False)
-    return best_so_far
-
-
 class Solver(BaseSolver):
     def _part1(self) -> Solution:
         return steps_to_end(self.grid, Point(1, 0), set())
 
     def _part2(self) -> Solution:
-        ans = steps_to_end_p2(
-            self.grid, Point(1, 0), self.grid.transform(lambda x: False)
+        junctions = {Point(1, 0), Point(self.grid.w - 2, self.grid.h - 1)}
+        for source, v in self.grid.iter():
+            if v == "#":
+                continue
+            num_valid_neighbors = 0
+            for n in self.grid.neighbors(source):
+                if n != "#":
+                    num_valid_neighbors += 1
+            if num_valid_neighbors > 2:
+                junctions.add(source)
+
+        graph = nx.DiGraph()
+        for source in junctions:
+            queue: deque[tuple[Point, int]] = deque([(source, 0)])
+            seen = set()
+            while queue:
+                current_pos, dist = queue.popleft()
+                if current_pos in seen:
+                    continue
+                seen.add(current_pos)
+                if current_pos != source and current_pos in junctions:
+                    graph.add_edge(source, current_pos, weight=dist)
+                    continue
+                for n, d in self.grid.neighbors_with_direction(current_pos):
+                    if n != "#":
+                        queue.append((current_pos.neighbor(d), dist + 1))
+
+        all_paths = nx.all_simple_paths(
+            graph, Point(1, 0), Point(self.grid.w - 2, self.grid.h - 1)
         )
+        ans = 0
+        for i, p in enumerate(all_paths):
+            ans = max(ans, nx.path_weight(graph, p, weight="weight"))
+            if i % 1000 == 0:
+                print(ans)
         return ans
