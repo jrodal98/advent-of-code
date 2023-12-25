dbf3efb45dca2b3c42adabde556f68d035349ba1
Jacob Rodal <jrodal@fb.com>
2023-12-25 00:58:16 -0500

    day 25

--- py2023/day25/solution.py
+++ py2023/day25/solution.py
@@ -8,12 +8,9 @@ import networkx as nx
 class Solver(BaseSolver):
     def _part1(self) -> Solution:
         graph = nx.Graph()
-        nodes = set()
         for line in self.data.splitlines():
             source, connected_to = line.split(": ")
-            nodes.add(source)
             for connected in connected_to.split():
-                nodes.add(connected)
                 graph.add_edge(source, connected)
 
         for edge in nx.minimum_edge_cut(graph):
