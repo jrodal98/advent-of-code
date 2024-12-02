7ee62123a61baa3e649f71ca61e29910c1ca07b7
Jacob Rodal <dev+github@jrodal.com>
2024-12-01 21:38:47 -0800

    part 2 refactor

--- py2024/day02/solution.py
+++ py2024/day02/solution.py
@@ -5,8 +5,7 @@ from aoc_utils.base_solver import BaseSolver, Solution
 
 
 class Solver(BaseSolver):
-    def _is_level_safe(self, level: str) -> bool:
-        nums = [int(n) for n in level.split()]
+    def _is_level_safe(self, nums: list[int]) -> bool:
         has_increased = False
         has_decreased = False
         for prev, cur in zip(nums, nums[1:]):
@@ -23,31 +22,16 @@ class Solver(BaseSolver):
         return True
 
     def _part1(self) -> Solution:
-        return sum(self._is_level_safe(level) for level in self.lines())
+        return sum(
+            self._is_level_safe([int(n) for n in level.split()])
+            for level in self.lines()
+        )
 
     def _part2(self) -> Solution:
-        safe = 0
-        for line in self.lines():
-            nums = [int(n) for n in line.split()]
-            for index_to_skip in range(len(nums) + 1):
-                nums_to_process = nums.copy()
-                if index_to_skip < len(nums):
-                    nums_to_process.pop(index_to_skip)
-                all_increasing = True
-                all_decreasing = True
-                safe_dropoff = True
-                last_num = None
-                for num in nums_to_process:
-                    if last_num is None:
-                        last_num = num
-                        continue
-                    all_increasing = all_increasing and (num > last_num)
-                    all_decreasing = all_decreasing and (num < last_num)
-                    diff = abs(num - last_num)
-                    safe_dropoff = safe_dropoff and diff in (1, 2, 3)
-                    last_num = num
-                if safe_dropoff and (all_increasing or all_decreasing):
-                    safe += 1
-                    break
-
-        return safe
+        return sum(
+            any(
+                self._is_level_safe(nums[:i] + nums[i + 1 :])
+                for i in range(len(nums) + 1)
+            )
+            for nums in ([int(n) for n in level.split()] for level in self.lines())
+        )
