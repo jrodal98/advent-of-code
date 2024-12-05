4a6ebbd7a41ad759c22b5c0eeb60a6b2c044fec3
Jacob Rodal <dev+github@jrodal.com>
2024-12-04 21:40:33 -0800

    part 2

--- py2024/day05/solution.py
+++ py2024/day05/solution.py
@@ -4,58 +4,60 @@
 from aoc_utils.base_solver import BaseSolver, Solution
 from collections import defaultdict
 
+import functools
+
 
 class Solver(BaseSolver):
     def _part1(self) -> Solution:
         rules, updates = self.data.split("\n\n")
-        violations = defaultdict(set)
+        rule_dict = defaultdict(set)
 
         for rule in rules.splitlines():
             before, after = rule.split("|")
-            violations[int(after)].add(int(before))
+            rule_dict[int(before)].add(int(after))
 
         ans = 0
         for update in updates.splitlines():
             nums = [int(n) for n in update.split(",")]
-            if self._is_in_order(nums, violations):
+            if self._is_in_order(nums, rule_dict):
                 ans += nums[len(nums) // 2]
         return ans
 
     def _is_in_order(
-        self, nums: list[int], violations: defaultdict[int, set[int]]
+        self, nums: list[int], rule_dict: defaultdict[int, set[int]]
     ) -> bool:
         for i, num in enumerate(nums):
             nums_after = nums[i + 1 :]
-            if not all(num in violations[n] for n in nums_after):
+            if any(num in rule_dict[n] for n in nums_after):
                 return False
         return True
 
     def _sort_nums(
-        self, nums: list[int], violations: defaultdict[int, set[int]]
+        self, nums: list[int], rule_dict: defaultdict[int, set[int]]
     ) -> list[int]:
-        new_nums = []
-        for i, num in enumerate(nums):
-            nums_after = nums[i + 1 :]
-            for n in nums_after:
-                if num in violations[n]:
-                    new_nums.append(n)
-                    break
-        return new_nums
+        def compare(a, b):
+            if b in rule_dict[a]:
+                return -1
+            else:
+                return 0
+
+        x = sorted(nums, key=functools.cmp_to_key(compare))
+        print(nums, x)
+        return x
 
     def _part2(self) -> Solution:
         rules, updates = self.data.split("\n\n")
-        violations = defaultdict(set)
+        rule_dict = defaultdict(set)
 
         for rule in rules.splitlines():
             before, after = rule.split("|")
-            violations[int(after)].add(int(before))
+            rule_dict[int(before)].add(int(after))
 
         ans = 0
         for update in updates.splitlines():
-            correctly_ordered = True
             nums = [int(n) for n in update.split(",")]
 
-            if not correctly_ordered:
-                nums = self._sort_nums(nums, violations)
+            if not self._is_in_order(nums, rule_dict):
+                nums = self._sort_nums(nums, rule_dict)
                 ans += nums[len(nums) // 2]
         return ans
