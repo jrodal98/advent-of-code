f437bf54c9345e7efbbe623572929ee2c0e1dc06
Jacob Rodal <dev+github@jrodal.com>
2024-12-05 23:53:03 -0800

    refactor

--- py2024/day06/solution.py
+++ py2024/day06/solution.py
@@ -4,21 +4,15 @@
 from aoc_utils.base_solver import BaseSolver, Solution
 from aoc_utils.point import Direction, Point
 
+import functools
+
 
 class Solver(BaseSolver):
     def _part1(self) -> Solution:
-        pos = self.grid.find("^")
-        dir = Direction.UP
-        seen = set()
-        while True:
-            seen.add(pos)
-            match self.grid.get_neighbor(pos, dir):
-                case None:
-                    return len(seen)
-                case "#":
-                    dir = dir.clockwise
-                case _:
-                    pos = pos.neighbor(dir)
+        return len(self._helper()[0])
+
+    def _part2(self) -> Solution:
+        return sum(self._check_if_loopable(pos, dir) for pos, dir in self._helper()[1])
 
     def _check_if_loopable(self, pos: Point, dir: Direction) -> bool:
         new_obstacle = pos.neighbor(dir)
@@ -26,11 +20,8 @@ class Solver(BaseSolver):
         seen = set()
         while (pos, dir) not in seen:
             seen.add((pos, dir))
-            if pos.neighbor(dir) == new_obstacle:
-                v = "#"
-            else:
-                v = self.grid.get_neighbor(pos, dir)
-            match v:
+            neighbor_pos = pos.neighbor(dir)
+            match "#" if neighbor_pos == new_obstacle else self.grid.get(neighbor_pos):
                 case None:
                     return False
                 case "#":
@@ -39,20 +30,17 @@ class Solver(BaseSolver):
                     pos = pos.neighbor(dir)
         return True
 
-    def _part2(self) -> Solution:
+    @functools.cache
+    def _helper(self) -> tuple[set[Point], set[tuple[Point, Direction]]]:
         pos = self.grid.find("^")
         dir = Direction.UP
         seen = set()
         check_for_loops = set()
-        ans = 0
         while True:
             seen.add(pos)
             match self.grid.get_neighbor(pos, dir):
                 case None:
-                    return sum(
-                        self._check_if_loopable(pos, dir)
-                        for pos, dir in check_for_loops
-                    )
+                    return seen, check_for_loops
                 case "#":
                     dir = dir.clockwise
                 case _:
