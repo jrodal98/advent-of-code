d3dce2582677d7ae80e8a393479636156f2620d5
Jacob Rodal <dev+github@jrodal.com>
2024-12-05 23:07:42 -0800

    part 2

--- py2024/day06/solution.py
+++ py2024/day06/solution.py
@@ -23,22 +23,25 @@ class Solver(BaseSolver):
     def _check_if_loopable(
         self, pos: Point, dir: Direction, seen_with_dir: set[tuple[Point, Direction]]
     ) -> bool:
-        if (pos, dir) in seen_with_dir:
-            return True
-        if self.grid.get_neighbor(pos, dir) is None:
-            return False
-        seen_with_dir.add((pos, dir))
-        if self.grid.get_neighbor(pos, dir, "#") == "#":
-            dir = dir.clockwise
-
-        return self._check_if_loopable(pos.neighbor(dir), dir, seen_with_dir)
+        while (pos, dir) not in seen_with_dir:
+            seen_with_dir.add((pos, dir))
+            match self.grid.get_neighbor(pos, dir):
+                case None:
+                    return False
+                case "#":
+                    dir = dir.clockwise
+                case _:
+                    pos = pos.neighbor(dir)
+        return True
 
     def _part2(self) -> Solution:
-        pos = self.grid.find("^")
+        starting_pos = self.grid.find("^")
+        pos = starting_pos
         dir = Direction.UP
         seen = set()
         seen_with_dir = set()
         new_obstructions = set()
+        allow_obstruction = True
         while True:
             seen.add(pos)
             seen_with_dir.add((pos, dir))
@@ -46,13 +49,19 @@ class Solver(BaseSolver):
                 case None:
                     return len(new_obstructions)
                 case "#":
+                    allow_obstruction = (
+                        self.grid.get(pos.neighbor(dir.counter_clockwise)) != "#"
+                    )
                     dir = dir.clockwise
                 case _:
                     new_pos = pos.neighbor(dir)
-                    self.grid.replace(new_pos, "#")
-                    if self._check_if_loopable(
-                        pos, dir.clockwise, seen_with_dir.copy()
+                    if (
+                        allow_obstruction
+                        and new_pos not in new_obstructions
+                        and new_pos != starting_pos
                     ):
-                        new_obstructions.add(pos.neighbor(dir))
-                    self.grid.replace(new_pos, ".")
+                        self.grid.replace(new_pos, "#")
+                        if self._check_if_loopable(pos, dir.clockwise, set()):
+                            new_obstructions.add(new_pos)
+                        self.grid.replace(new_pos, ".")
                     pos = new_pos
