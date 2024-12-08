c339d71e1af3316df77f5eaf917aa9a8bd167c42
Jacob Rodal <dev+github@jrodal.com>
2024-12-07 22:22:55 -0800

    part 2

--- py2024/day08/solution.py
+++ py2024/day08/solution.py
@@ -25,4 +25,30 @@ class Solver(BaseSolver):
         return len(antinodes)
 
     def _part2(self) -> Solution:
-        raise NotImplementedError
+        positions = defaultdict(list)
+        for p, v in self.grid.iter(disqualify="."):
+            positions[v].append(p)
+
+        antinodes = set()
+        for all_points in positions.values():
+            for p1, p2 in combinations(all_points, 2):
+                antinodes.add(p1)
+                antinodes.add(p2)
+                dx = p2.x - p1.x
+                dy = p2.y - p1.y
+                points = [p1, p2]
+                visited = set()
+                while points:
+                    new_points = []
+                    for p in points:
+                        visited.add(p)
+                        for m in (1, -1):
+                            new_point = p.translate(m * dx, m * dy)
+                            if new_point not in visited and self.grid.inbounds(
+                                new_point
+                            ):
+                                new_points.append(new_point)
+                                antinodes.add(new_point)
+                                visited.add(new_point)
+                    points = new_points
+        return len(antinodes)
