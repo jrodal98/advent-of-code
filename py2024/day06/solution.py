5d045fa33e7928e0cdceeb08221d72fd34c9a55b
Jacob Rodal <dev+github@jrodal.com>
2024-12-08 20:57:18 -0800

    animate day 6

--- py2024/day06/solution.py
+++ py2024/day06/solution.py
@@ -18,6 +18,7 @@ class Solver(BaseSolver):
         cycle: bool
 
     def _part1(self) -> Solution:
+        self._set_animation_grid()
         return len(self.walk(self.grid, self.grid.find("^"), Direction.UP).seen)
 
     def _part2(self) -> Solution:
@@ -36,9 +37,8 @@ class Solver(BaseSolver):
                 )
             )
 
-    @classmethod
     def walk(
-        cls, grid: Grid, pos: Point, dir: Direction, new_obstacle: Point | None = None
+        self, grid: Grid, pos: Point, dir: Direction, new_obstacle: Point | None = None
     ) -> "Solver.Result":
         seen = set()
         seen_with_dir = set()
@@ -49,12 +49,16 @@ class Solver(BaseSolver):
             neighbor_pos = pos.neighbor(dir)
             match "#" if neighbor_pos == new_obstacle else grid.get(neighbor_pos):
                 case None:
-                    return cls.Result(seen, possible_obstacles, False)
+                    return self.Result(seen, possible_obstacles, False)
                 case "#":
                     dir = dir.clockwise
                 case _:
                     new_pos = pos.neighbor(dir)
                     if new_pos not in seen:
                         possible_obstacles.add((pos, dir))
+                    self._update_animation(
+                        point=new_pos,
+                        value=dir.arrow,
+                    )
                     pos = new_pos
-        return cls.Result(seen, possible_obstacles, True)
+        return self.Result(seen, possible_obstacles, True)
