caf1a28d210573b05fceceb06829e164292b5c85
Jacob Rodal <dev+github@jrodal.com>
2024-12-08 23:00:55 -0800

    refactor

--- py2024/day09/solution.py
+++ py2024/day09/solution.py
@@ -5,27 +5,11 @@ from aoc_utils.base_solver import BaseSolver, Solution
 
 
 class Solver(BaseSolver):
-    def push_nones_to_back(self, lst: list[int]):
-        left = 0
-        right = len(lst) - 1
-
-        while left < right:
-            while left < right and lst[left] is not None:
-                left += 1
-            while left < right and lst[right] is None:
-                right -= 1
-            if left < right:
-                lst[left], lst[right] = lst[right], lst[left]
-                left += 1
-                right -= 1
-
-        return lst
-
     def _part1(self) -> Solution:
         numbers = [int(n) for n in self.data]
         files_counts, free_spaces = numbers[::2], numbers[1::2]
         if len(files_counts) != len(free_spaces):
-            free_spaces = free_spaces + [0]
+            free_spaces.append(0)
         res = []
         for i, (files_count, free_space) in enumerate(zip(files_counts, free_spaces)):
             for _ in range(files_count):
@@ -33,19 +17,26 @@ class Solver(BaseSolver):
             for _ in range(free_space):
                 res.append(None)
 
-        res = [x or 0 for x in self.push_nones_to_back(res)]
+        left = 0
+        right = len(res) - 1
 
-        ans = 0
-        for i, v in enumerate(res):
-            ans += i * v
+        while left < right:
+            while left < right and res[left] is not None:
+                left += 1
+            while left < right and res[right] is None:
+                right -= 1
+            if left < right:
+                res[left], res[right] = res[right], res[left]
+                left += 1
+                right -= 1
 
-        return ans
+        return sum(i * (v or 0) for i, v in enumerate(res))
 
     def _part2(self) -> Solution:
         numbers = [int(n) for n in self.data]
         files_counts, free_spaces = numbers[::2], numbers[1::2]
         if len(files_counts) != len(free_spaces):
-            free_spaces = free_spaces + [0]
+            free_spaces.append(0)
         res = []
         all_nums = []
         for i, (files_count, free_space) in enumerate(zip(files_counts, free_spaces)):
@@ -67,9 +58,6 @@ class Solver(BaseSolver):
                     res[i : i + window_size] = [id] * window_size
                     break
 
-        print(res)
-        ans = 0
-        for i, v in enumerate(res):
-            ans += i * (v or 0)
+        return sum(i * (v or 0) for i, v in enumerate(res))
 
         return ans
