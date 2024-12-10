e2eee2e45bba8ac30b09a079490d987973d428c0
Jacob Rodal <dev+github@jrodal.com>
2024-12-09 21:36:16 -0800

    part 2

--- py2024/day10/solution.py
+++ py2024/day10/solution.py
@@ -8,32 +8,38 @@ from collections import defaultdict
 
 
 class Solver(BaseSolver):
-    def reachable(
+    def _part1(self) -> Solution:
+        return self._helper(False)
+
+    def _part2(self) -> Solution:
+        return self._helper(True)
+
+    def _reachable(
         self,
         grid: Grid[int],
         start_pos: Point,
         start_val: int,
         finish: Point,
-        graph: dict[Point, dict[Point, bool]],
-    ) -> bool:
+        graph: dict[Point, dict[Point, int]],
+    ) -> int:
         if start_pos == finish:
-            graph[start_pos][finish] = True
-            return True
+            graph[start_pos][finish] = 1
+            return 1
 
         if finish in graph[start_pos]:
             return graph[start_pos][finish]
+
+        graph[start_pos][finish] = 0
         for neighbor_pos, neighbor_val, _ in grid.neighbors(start_pos):
             if neighbor_val - start_val != 1:
                 continue
-            if self.reachable(grid, neighbor_pos, neighbor_val, finish, graph):
-                graph[start_pos][finish] = True
-                return True
+            graph[start_pos][finish] += self._reachable(
+                grid, neighbor_pos, neighbor_val, finish, graph
+            )
 
-        graph[start_pos][finish] = False
+        return graph[start_pos][finish]
 
-        return False
-
-    def _part1(self) -> Solution:
+    def _helper(self, is_part_2: bool) -> int:
         grid = self.grid.transform(int)
         trailhead_positions = list(grid.findall(0))
         peak_positions = list(grid.findall(9))
@@ -41,9 +47,9 @@ class Solver(BaseSolver):
         graph = defaultdict(dict)
         for peak in peak_positions:
             for trailhead in trailhead_positions:
-                if self.reachable(grid, trailhead, 0, peak, graph):
+                score = self._reachable(grid, trailhead, 0, peak, graph)
+                if is_part_2:
+                    ans += score
+                elif score:
                     ans += 1
         return ans
-
-    def _part2(self) -> Solution:
-        raise NotImplementedError
