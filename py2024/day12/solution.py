fb086986d19c20868c4a28e6521d0ff893a379e1
Jacob Rodal <dev+github@jrodal.com>
2024-12-12 01:06:57 -0800

    part 2

--- py2024/day12/solution.py
+++ py2024/day12/solution.py
@@ -25,64 +25,70 @@ class Solver(BaseSolver):
 
         return regions
 
-    def _score_region_part1(self, region: set[Point]) -> int:
-        area = len(region)
-        perimeter = area * 4
-        for p in region:
-            for neighbor in p.neighbors():
-                if neighbor in region:
-                    perimeter -= 1
-        return area * perimeter
-
-    def _score_region_part2(self, region: set[Point]) -> int:
-        area = len(region)
-
-        perimeter: set[Point] = set()
-        for p in region:
-            for neighbor in p.neighbors(include_diagonal=True):
-                if neighbor not in region:
-                    perimeter.add(neighbor)
-
-        num_sides = 0
-        for p in perimeter:
-            exterior_corners = 0
-            if p.left in perimeter and p.down in perimeter and p.bottom_left in region:
-                exterior_corners += 1
-
-            if (
-                p.right in perimeter
-                and p.down in perimeter
-                and p.bottom_right in region
-            ):
-                exterior_corners += 1
-
-            if p.left in perimeter and p.up in perimeter and p.upper_left in region:
-                exterior_corners += 1
-
-            if p.right in perimeter and p.up in perimeter and p.upper_right in region:
-                exterior_corners += 1
-
-            num_sides += exterior_corners
-
-            # count interior corners
-            if p.left in region and p.down in region:
-                num_sides += 1
-
-            if p.right in region and p.down in region:
-                num_sides += 1
-
-            if p.left in region and p.up in region:
-                num_sides += 1
-
-            if p.right in region and p.up in region:
-                num_sides += 1
-
-        return area * num_sides
-
     def _part1(self) -> Solution:
         regions = self._extract_regions()
-        return sum(self._score_region_part1(r) for r in regions)
+        score = 0
+        for region in regions:
+            area = len(region)
+            perimeter = area * 4
+            for p in region:
+                for neighbor in p.neighbors():
+                    if neighbor in region:
+                        perimeter -= 1
+            score += area * perimeter
+        return score
 
     def _part2(self) -> Solution:
         regions = self._extract_regions()
-        return sum(self._score_region_part2(r) for r in regions)
+        score = 0
+        for region in regions:
+            area = len(region)
+
+            perimeter: set[Point] = set()
+            for p in region:
+                for neighbor in p.neighbors(include_diagonal=True):
+                    if neighbor not in region:
+                        perimeter.add(neighbor)
+
+            num_sides = 0
+            for p in perimeter:
+                ############################### EXTERIOR CORNERS ####################################
+                if (
+                    p.left in perimeter
+                    and p.down in perimeter
+                    and p.bottom_left in region
+                ):
+                    num_sides += 1
+
+                if (
+                    p.right in perimeter
+                    and p.down in perimeter
+                    and p.bottom_right in region
+                ):
+                    num_sides += 1
+
+                if p.left in perimeter and p.up in perimeter and p.upper_left in region:
+                    num_sides += 1
+
+                if (
+                    p.right in perimeter
+                    and p.up in perimeter
+                    and p.upper_right in region
+                ):
+                    num_sides += 1
+
+                ############################### INTERIOR CORNERS ####################################
+                if p.left in region and p.down in region:
+                    num_sides += 1
+
+                if p.right in region and p.down in region:
+                    num_sides += 1
+
+                if p.left in region and p.up in region:
+                    num_sides += 1
+
+                if p.right in region and p.up in region:
+                    num_sides += 1
+
+            score += area * num_sides
+        return score
