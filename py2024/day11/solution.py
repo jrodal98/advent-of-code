9827f11428c6f3915f79de8859eac797944ded1a
Jacob Rodal <dev+github@jrodal.com>
2024-12-12 23:28:49 -0800

    day 11

--- py2024/day11/solution.py
+++ py2024/day11/solution.py
@@ -8,22 +8,26 @@ from functools import cache
 
 class Solver(BaseSolver):
     def _solve(self, part1: bool) -> Solution:
-        return sum(_num_stones(stone, 25 if part1 else 75) for stone in ints(self.data))
+        return sum(
+            self._num_stones(stone, 25 if part1 else 75) for stone in ints(self.data)
+        )
 
+    @classmethod
+    @cache
+    def _num_stones(cls, stone: int, blinks: int) -> int:
+        if blinks == 0:
+            return 1
 
-@cache
-def _num_stones(stone: int, blinks: int) -> int:
-    if blinks == 0:
-        return 1
+        blinks -= 1
 
-    blinks -= 1
+        if stone == 0:
+            return cls._num_stones(1, blinks)
 
-    if stone == 0:
-        return _num_stones(1, blinks)
+        stone_str = str(stone)
+        left, right = stone_str[: len(stone_str) // 2], stone_str[len(stone_str) // 2 :]
+        if len(left) == len(right):
+            return cls._num_stones(int(left), blinks) + cls._num_stones(
+                int(right), blinks
+            )
 
-    stone_str = str(stone)
-    left, right = stone_str[: len(stone_str) // 2], stone_str[len(stone_str) // 2 :]
-    if len(left) == len(right):
-        return _num_stones(int(left), blinks) + _num_stones(int(right), blinks)
-
-    return _num_stones(stone * 2024, blinks)
+        return cls._num_stones(stone * 2024, blinks)
