2a11e6d5ff5579a8d7167a1971c13c891210064b
Jacob Rodal <dev+github@jrodal.com>
2024-12-15 22:44:38 -0800

    day 16

--- py2024/day16/solution.py
+++ py2024/day16/solution.py
@@ -4,50 +4,42 @@
 from aoc_utils.base_solver import BaseSolver, Solution
 from aoc_utils.grid import Grid
 from aoc_utils.point import Direction, Point
-
-MAX_COST = 10000000000000000000000000000000000000000000
-
-import sys
-
-sys.setrecursionlimit(100000)
-
-
-def _min_cost(
-    grid: Grid,
-    direction: Direction,
-    start: Point,
-    end: Point,
-    seen: dict[tuple[Point, Direction], tuple[int, int]],
-    cost: int = 0,
-) -> int:
-    if start == end:
-        return cost
-    min_cost = MAX_COST
-    key = (start, direction)
-    if key in seen:
-        seen_acc_cost, min_cost = seen[key]
-        if cost > seen_acc_cost:
-            return min_cost
-
-    seen[key] = (cost, min_cost)
-    if grid.at(neighbor := start.neighbor(direction)) != "#":
-        min_cost = min(
-            min_cost, _min_cost(grid, direction, neighbor, end, seen, cost + 1)
-        )
-
-    for d in [
-        direction.counter_clockwise,
-        direction.clockwise,
-    ]:
-        if min_cost > cost + 1000:
-            min_cost = min(min_cost, _min_cost(grid, d, start, end, seen, cost + 1000))
-
-    seen[key] = (cost, min_cost)
-    return min_cost
+import networkx as nx
 
 
 class Solver(BaseSolver):
     def _solve(self, part1: bool) -> Solution:
-        start_node = self.grid.find("S")
+        graph = nx.Graph()
+        for p, _ in self.grid.iter(exclude="#"):
+            for neighbor_p, _, direction in self.grid.neighbors(p, exclude="#"):
+                graph.add_edge((p, direction), (neighbor_p, direction), weight=1)
+                graph.add_edge(
+                    (p, direction), (neighbor_p, direction.clockwise), weight=1001
+                )
+                graph.add_edge(
+                    (p, direction),
+                    (neighbor_p, direction.counter_clockwise),
+                    weight=1001,
+                )
+
+        source = (self.grid.find("S"), Direction.RIGHT)
         end_node = self.grid.find("E")
-        return _min_cost(self.grid, Direction.RIGHT, start_node, end_node, {}, 0)
+        target = (end_node, Direction.UPPER_RIGHT)
+
+        for d in Direction.dir4():
+            graph.add_edge((end_node, d), target, weight=0)
+
+        if part1:
+            return nx.shortest_path_length(
+                graph, source=source, target=target, weight="weight"
+            )
+        else:
+            return len(
+                {
+                    pos
+                    for path in nx.all_shortest_paths(
+                        graph, source=source, target=target, weight="weight"
+                    )
+                    for pos, _ in path
+                }
+            )
