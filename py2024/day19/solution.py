077e25870e4d4dcf6b13980b30967466d9e40b4d
Jacob Rodal <dev+github@jrodal.com>
2024-12-19 20:53:51 -0800

    day 19

--- py2024/day19/solution.py
+++ py2024/day19/solution.py
@@ -9,24 +9,27 @@ class Solver(BaseSolver):
     @classmethod
     @cache
     def _num_ways(cls, towel_patterns: frozenset[str], design: str) -> int:
-        if not design:
-            return 1
-        num_ways = 0
-        for pattern in towel_patterns:
-            if design.startswith(pattern):
-                num_ways += cls._num_ways(towel_patterns, design[len(pattern) :])
-
-        return num_ways
+        return (
+            1
+            if not design
+            else sum(
+                cls._num_ways(towel_patterns, design[len(pattern) :])
+                for pattern in towel_patterns
+                if design.startswith(pattern)
+            )
+        )
 
     def _solve(self, part1: bool) -> Solution:
         towel_patterns, desired_designs = self.sections()
         towel_patterns = frozenset(towel_patterns.split(", "))
+
         part1_ans = 0
         part2_ans = 0
         for design in desired_designs.split("\n"):
             num_ways = self._num_ways(towel_patterns, design)
             part1_ans += bool(num_ways)
             part2_ans += num_ways
+
         if part1:
             return part1_ans
         else:
