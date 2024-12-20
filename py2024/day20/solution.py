9d875ada5f7491ac5f809297a10b5affa3086294
Jacob Rodal <dev+github@jrodal.com>
2024-12-20 01:52:55 -0800

    day 20 part 2 slowish

--- py2024/day20/solution.py
+++ py2024/day20/solution.py
@@ -7,21 +7,16 @@ from itertools import combinations
 
 class Solver(BaseSolver):
     def _solve(self, part1: bool) -> Solution:
+        min_cheat = (
+            2 if part1 and self._is_unit_test else 50 if self._is_unit_test else 100
+        )
+        max_cheat = 2 if part1 else 20
         return sum(
-            1
+            (
+                (cheat := n1.manhattan_distance(n2)) <= max_cheat
+                and cheat <= (b - a) - min_cheat
+            )
             for (a, n1), (b, n2) in combinations(
                 enumerate(self.grid.shortest_path("S", "E", exclude="#")), 2
-            )
-            if b - a
-            > (
-                cheat_min_save := 2
-                if part1 and self._is_unit_test
-                else 50
-                if self._is_unit_test
-                else 100
-            )
-            and (
-                (cheat_length := n1.manhattan_distance(n2)) <= (2 if part1 else 20)
-                and cheat_length <= (b - a) - cheat_min_save
             )
         )
