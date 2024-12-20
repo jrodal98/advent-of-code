b4b4fad4e0cadff7c7d7fdf6254e0cc6baf23f34
Jacob Rodal <dev+github@jrodal.com>
2024-12-19 20:50:31 -0800

    day 19

---
+++ py2024/day19/solution.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# www.jrodal.com
+
+from aoc_utils.base_solver import BaseSolver, Solution
+from functools import cache
+
+
+class Solver(BaseSolver):
+    @classmethod
+    @cache
+    def _num_ways(cls, towel_patterns: frozenset[str], design: str) -> int:
+        if not design:
+            return 1
+        num_ways = 0
+        for pattern in towel_patterns:
+            if design.startswith(pattern):
+                num_ways += cls._num_ways(towel_patterns, design[len(pattern) :])
+
+        return num_ways
+
+    def _solve(self, part1: bool) -> Solution:
+        towel_patterns, desired_designs = self.sections()
+        towel_patterns = frozenset(towel_patterns.split(", "))
+        part1_ans = 0
+        part2_ans = 0
+        for design in desired_designs.split("\n"):
+            num_ways = self._num_ways(towel_patterns, design)
+            part1_ans += bool(num_ways)
+            part2_ans += num_ways
+        if part1:
+            return part1_ans
+        else:
+            return part2_ans
