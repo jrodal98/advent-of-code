16349bebe833f742d884c72eb465db41b931858a
Jacob Rodal <jrodal@meta.com>
2025-12-01 15:28:13 -0800

    2025 day 1

--- py2025/day01/solution.py
+++ py2025/day01/solution.py
@@ -6,23 +6,29 @@ from aoc_utils.base_solver import BaseSolver, Solution
 
 class Solver(BaseSolver):
     def _solve(self, part1: bool) -> Solution:
-        pos = 50
-        modulo = 100
-        num_zeros = 0
-        lines = self.lines()
-        for line in lines:
-            if line[0] == "L":
-                raw_pos = pos - int(line[1:])
-                if not part1:
+        STARTING_POS = 50
+        MODULO = 100
+
+        pos = STARTING_POS
+        zero_crossings = 0
+
+        for line in self.lines():
+            direction = line[0]
+            value = int(line[1:])
+
+            raw_pos = pos - value if direction == "L" else pos + value
+
+            if not part1:
+                if direction == "L":
                     if pos != 0 and raw_pos <= 0:
-                        num_zeros += 1
-                    num_zeros += abs(int(raw_pos / modulo))
-                pos = raw_pos % modulo
-            else:
-                raw_pos = pos + int(line[1:])
-                if not part1:
-                    num_zeros += int(raw_pos / modulo)
-                pos = raw_pos % modulo
+                        zero_crossings += 1
+                    zero_crossings += abs(int(raw_pos / MODULO))
+                else:
+                    zero_crossings += int(raw_pos / MODULO)
+
+            pos = raw_pos % MODULO
+
             if part1 and pos == 0:
-                num_zeros += 1
-        return num_zeros
+                zero_crossings += 1
+
+        return zero_crossings
